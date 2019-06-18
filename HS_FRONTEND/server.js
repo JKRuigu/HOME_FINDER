30e2a9587b9edd6299f80b093e00d3ca925bb54f
JK Ruigu <ruigukjohn@gmail.com>
2019-06-18 18:08:21 +0300

    Intial commit with ejs

---
+++ HS_FRONTEND/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//ROUTES
+app.get('/',(req,res)=>{
+	res.render('index')
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)});
